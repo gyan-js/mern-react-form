476f4d3446238b593af2c3a028ea1476f57777c8
Gyan <gyan0304@gmail.com>
2023-03-25 04:18:35 -0400

    feat: Created and designed the form page ✨

---
+++ client/src/pages/Form.js
@@ -0,0 +1,34 @@
+import React from 'react'
+
+const Form = () => {
+  return (
+    <div className=' min-h-screen bg-white overflow-hidden'>
+       <div className=' text-xl py-28'>
+        <h1 className='text-4xl font-bold text-[#5469d4] text-center font-mono'>WELCOME USER</h1>
+    <div className='mt-[10px] w-full mx-w-md rounded shadow-[rgba(60,66,87,0.12)_0px_7px_14px_0px,rgba(0,0,0,0.12)_0px_3px_6px_0px] mx-auto my-0 max-w-[448px]' >
+        <div className='flex flex-col drop-shadow-md form  max-w-[448px] w-[100%]  p-[30px] pb-[48px] '>
+            <div className='field pb-[24px] flex flex-col '>
+                <label className='text-[#000]  text-[14px] '>FULL NAME</label>
+                <input className='text-[16px] leading-[24px] min-h-[44px] rounded-[4px] outline-none border-none px-3 w-[380px]' maxLength="200" required placeholder='Enter your full name*' />
+            </div>
+            <div className='field pb-[24px] flex flex-col '>
+                <label className='text-[#000]  text-[14px] '>USERNAME</label>
+                <input className='text-[16px] leading-[24px] min-h-[44px] rounded-[4px] outline-none border-none px-3 w-[380px]' maxLength="200" required placeholder='Enter your username*'/>
+            </div>
+            <div className='field pb-[24px] flex flex-col '>
+                <label className='text-[#000]  text-[14px] '>EMAIL</label>
+                <input className='text-[16px] leading-[24px] min-h-[44px] rounded-[4px] outline-none border-none px-3 w-[380px]' maxLength="200" required placeholder='Enter your email*' type="email" />
+            </div>
+            <div className='field pb-[24px] flex flex-col '>
+                <label className='text-[#000]  text-[14px] '>PASSWORD</label>
+                <input className='text-[16px] leading-[24px] min-h-[44px] rounded-[4px] outline-none border-none px-3 w-[380px]' maxLength="200" required placeholder='Set your paasword*'  type="password"/>
+            </div>
+            <div className='field w-[100%] h-[50px] bg-[#5469d4] text-white border-none ouline-none rounded-[20px] flex justify-center text-center py-[10px] font-sans font-bold cursor-pointer hover:scale-105 '> SIGN UP</div>
+        </div>
+    </div>
+       </div>
+    </div>
+  )
+}
+
+export default Form
